229b9ab528efc183187fb0620e4187754c943bd9
Dennys Morillo <dennyoscar13@gmail.com>
2022-05-22 19:43:56 -0400

    Fixed nord_minimal theme

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -39,7 +39,8 @@ vim.g['NERDTreeQuitOnOpen'] = 1
 vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g['airline#extensiones#tabline#left_sep'] = ''
 vim.g['airline#extensiones#tabline#left_sep'] = ''
-----------------------------------------------------------
+vim.g['airline_theme'] = 'nord_minimal'
+------------------------------------------------------------
 
 
 
